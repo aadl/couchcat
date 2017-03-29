04e29456a7b1d9c428e691afb1b20f6cd7d6a3a3
Ryan Eby <eby@ryaneby.com>
2017-03-29 14:31:14 -0400

    Process negatives every minute if not already running

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\ProcessNegatives::class,
     ];
 
     /**
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        // Process negatives every minute if not still running
+        $schedule->command('process:negatives')->withoutOverlapping();
     }
 
     /**
