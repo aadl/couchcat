12b5bc3e53ae9cffa902821174197eddcf27b23e
Ryan Eby <eby@ryaneby.com>
2017-07-23 15:57:34 -0400

    Command to invalidate an array of paths in cloudfront

---
+++ app/Console/Commands/AwsInvalidate.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Couchcat\Console\Commands;
+
+use AWS;
+use Illuminate\Console\Command;
+
+class AwsInvalidate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'aws:invalidate { paths* : Array of paths to invalidate }';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Invalidate Cloudfront Paths';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $invalidation_paths = $this->argument('paths');
+
+        $cloudfront = AWS::createClient('cloudfront');
+        $cloudfront->createInvalidation([
+            'DistributionId' => env('AWS_CLOUDFRONT_DISTRIBUTION'),
+            'InvalidationBatch' => [ 
+                'CallerReference' => (string)time(),
+                'Paths' => [
+                    'Items' => $invalidation_paths,
+                    'Quantity' => count($invalidation_paths),
+                ],
+            ],
+        ]);
+    }
+}
