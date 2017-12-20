2345806165196befed7823c2193832d609e35d2c
Ryan Eby <eby@ryaneby.com>
2017-12-20 18:00:07 -0500

    add flac zip creation script

--- app/Processors/Music/Flac.php
+++ app/Processors/Music/Flac.php
@@ -3,13 +3,14 @@
 namespace Couchcat\Processors\Music;
 
 use Illuminate\Support\Facades\Storage;
+use Comodojo\Zip\Zip;
 
 class Flac
 {
     /**
      * Rename the flac files to be URL friendly
      *
-     * @return void
+     * @return bool
      */
     public function fixFlacFilename($file)
     {
@@ -26,5 +27,16 @@ class Flac
         } else {
             return false;
         }
+    }
+
+    public function createFlacZip($couchid)
+    {
+        $flac_location = storage_path('app/music/'.$couchid .'/data');
+        $zip_location = storage_path('app/music/'.$couchid .'/derivatives/');
+        $zip_filename = $zip_location . $couchid . '-flac.zip';
+        $zip = Zip::create($zip_filename);
+        $zip->setSkipped('HIDDEN');
+        $zip->add($flac_location, true);
+        return $zip->close();
     }
 }
