2c82e5c3338a847e710cfcb1005769fb36a78d11
Ryan Eby <eby@ryaneby.com>
2018-01-24 16:57:00 -0500

    basic cover cache command

---
+++ app/Console/Commands/CacheCovers.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Couchcat\Console\Commands;
+
+use Couchcat\Libraries\Syndetics;
+use Couchcat\Libraries\CoverCache;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use Illuminate\Console\Command;
+
+class CacheCovers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cache:covers { record_ids* : Array of record ids to cache covers for }';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Cache a cover for a bib';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->covercache = new CoverCache;
+        $this->syndetics = new Syndetics('anarp');
+        $this->guzzle = new Client();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $record_ids = $this->argument('record_ids');
+        foreach ($record_ids as $record_id) {
+            try {
+                $cover_check = $this->guzzle->request('GET', 'https://aadl.org/files/covers/'.$record_id.'.jpg');
+            } catch (RequestException $e) {
+                $response = $this->guzzle->request('GET', 'https://api.aadl.org/record/'.$record_id);
+                $record = json_decode($response->getBody());
+                if (isset($record->stdnum)) {
+                    if(is_array($record->stdnum)) {
+                        $isbn = $record->stdnum[0];
+                    } else {
+                        $isbn = $record->stdnum;
+                    }
+                    if ($coverurl = $this->syndetics->getIsbn($isbn)->getCoverUrl()) {
+                        $this->covercache->saveCover($coverurl, $record_id);
+                        $this->covercache->uploadCover($record_id.'.jpg');
+                        $this->call('aws:invalidate', ['paths' => ['/cover/200/'.$record_id.'.jpg']]);
+                    }
+                }
+            }
+        }
+    }
+}
