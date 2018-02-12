ab20f4bd1e52df4da4075f842b4cf7a80b760942
Ryan Eby <eby@ryaneby.com>
2018-02-11 20:45:25 -0500

    route cache middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,5 +57,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \Couchcat\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'cache' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
     ];
 }
