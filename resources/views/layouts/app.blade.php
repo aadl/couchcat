694fce9fe1c4db60d2c96f24463f1031ccc8a3fa
Ryan Eby <eby@ryaneby.com>
2018-03-14 14:59:18 -0400

    bootstrap nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,27 +1,27 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
     <head>
         <title>Couchcat - @yield('title')</title>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css" integrity="sha256-t2/7smZfgrST4FS1DT0bs/KotCM74XlcqZN5Vu7xlrw=" crossorigin="anonymous" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
     <body>
-        <header>
-            <nav>
-                <ul>
-                    <li><a href="/license">Licenses</a></li>
-                </ul>
+        <header class="mb-3 fixed-top">
+            <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item"><a class="navbar-brand active" href="/">Home</a></li>
+              <li class="nav-item"><a class="nav-link" href="/vendors">Vendors</a></li>
+              <li class="nav-item"><a class="nav-link" href="/licenses">Licenses</a></li>
+            </ul>
             </nav>
         </header>
-        <div class="container">
+        <main role="main" class="container">
             @yield('content')
-        </div>
+        </main>
         <script
-              src="https://code.jquery.com/jquery-2.2.4.min.js"
-              integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
-              crossorigin="anonymous">
+              src="{{ mix('js/app.js') }}">
         </script>
     </body>
 </html>
