3df441de250fe02557c54a51d5b98fe4a4d46485
Ryan Eby <eby@ryaneby.com>
2018-04-11 15:54:26 -0400

    easier to read card format

--- resources/views/vendor/show.blade.php
+++ resources/views/vendor/show.blade.php
@@ -4,16 +4,37 @@
 
 @section('content')
 
-<h1>{{ $vendor->name }} (<a href="{{ route('vendor.edit',['id' => $vendor['id']]) }}">Edit</a>)</h1>
-<hr>
-<h4>Contact Information</h4>
-<p>{{ $vendor->contact_name}}</p>
-<p>{{ $vendor->contact_email}}</p>
-<p>{!! nl2br(e($vendor->contact_address)) !!}</p>
-<h4>Notes</h4>
-<p>{!! nl2br(e($vendor->notes)) !!}</p>
-<h4>History</h4>
-<p>Created at: {{ $vendor->created_at->toDayDateTimeString()}}</p>
-<p>Updated at: {{ $vendor->updated_at->toDayDateTimeString()}}</p>
-
+<h1>{{ $vendor->name }} <a href="{{ route('vendor.edit',['id' => $vendor['id']]) }}" class="btn btn-outline-primary float-right">Edit Vendor</a></h1>
+<div class="card">
+    <h4 class="card-header">Contact Information</h4>
+    <div class="card-body">
+        <p>{{ $vendor->contact_name}}</p>
+        <p>{{ $vendor->contact_email}}</p>
+        <p>{!! nl2br(e($vendor->contact_address)) !!}</p>
+    </div>
+</div>
+<div class="card mt-3">
+    <h4 class="card-header">Licenses</h4>
+    <div class="card-body">
+        <ul>
+        @foreach($vendor->licenses as $license)
+            <li><a href="{{ route('license.show',['id' => $license->id]) }}">{{ $license->statistics_stub }}</a> (expires {{ $license->expires }})</li>
+        @endforeach
+        </ul>
+    </div>
+</div>
+<div class="card mt-3">
+    <h4 class="card-header">Notes</h4>
+    <div class="card-body">
+        <p>{!! nl2br(e($vendor->notes)) !!}</p>
+    </div>
+</div>
+<div class="card mt-3">
+    <h4 class="card-header">History</h4>
+    <div class="card-body">
+        <p>Created at: {{ $vendor->created_at->toDayDateTimeString()}}</p>
+        <p>Updated at: {{ $vendor->updated_at->toDayDateTimeString()}}</p>
+    </div>
+</div>
+<a href="{{ route('vendor.edit',['id' => $vendor['id']]) }}" class="btn btn-outline-primary float-right mt-2">Edit Vendor</a>
 @endsection
