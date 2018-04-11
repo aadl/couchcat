467c0b088ce598aad11c8d164bc13abeceb08de0
Ryan Eby <eby@ryaneby.com>
2018-04-11 15:54:06 -0400

    display record count

--- resources/views/license/index.blade.php
+++ resources/views/license/index.blade.php
@@ -12,19 +12,25 @@
 		<th scope="col">Starts</th>
 		<th scope="col">Expires</th>
 		<th scope="col">Patrons Only</th>
-		<th scope="col">Works</th>
+		<th scope="col">Records</th>
 		<th scope="col">Actions</th>
 		</tr>
 	</thead>
 	<tbody>
 	@foreach($licenses as $license)
+		@php 
+			$records_count = 0;
+			if ($records_key = array_search($license->statistics_stub, array_column($records, 'key'))) {
+				$records_count = $records[$records_key]->value;
+			}
+		@endphp
 		<tr>
-			<td><a href="/vendor/{{ $license['vendor']['id'] }}">{{ $license['vendor']['name'] }}</a></td>
+			<td><a href="{{ route('vendor.show',['id' => $license['vendor']['id']]) }}">{{ $license['vendor']['name'] }}</a></td>
 			<td>{{ $license['statistics_stub'] }}</td>
 			<td>{{ $license['starts'] }}</td>
 			<td class='{{ $license->expired ? 'text-danger' : 'text-success' }}'>{{ $license['expires'] }}</td>
 			<td>{{ $license['patrons_only'] ? 'yes' : 'no' }}</td>
-			<td>-</td>
+			<td>{{ $records_count }}</td>
 			<td><a href="{{ route('license.edit',['id' => $license['id']]) }}">Edit</a></td>
 		</tr>
 	@endforeach
