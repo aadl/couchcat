c04f79d25cb943d607ee8d02077302fc1629752c
Ryan Eby <eby@ryaneby.com>
2018-04-12 13:09:17 -0400

    fixed creation form

--- resources/views/license/create.blade.php
+++ resources/views/license/create.blade.php
@@ -3,44 +3,61 @@
 @section('title', 'Add a License')
 
 @section('content')
-
+@if (count($errors) > 0)
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 {{ Form::open(array('route' => 'license.store')) }}
 <div class="form-group row">
-    <label for="vendor" class="col-sm-2 col-form-label">Vendor Name</label>
+    <label for="vendor_id" class="col-sm-2 col-form-label">Vendor Name</label>
     <div class="col-sm-6">
-        <select type="text" class="form-control" id="vendor" aria-describedby="vendorHelp" placeholder="My Vendor">
+        <select type="text" class="form-control" name="vendor_id" aria-describedby="vendorHelp" placeholder="My Vendor">
             @foreach ($vendors as $vendor)
-                <option id="{{ $vendor->id }}">{{ $vendor->name }}</option>
+                <option value="{{ $vendor->id }}">{{ $vendor->name }}</option>
             @endforeach
         </select>
         <small id="emailHelp" class="form-text text-muted">Create new vendors on the vendor page.</small>
     </div>
 </div>
 <div class="form-group row">
-    <label for="satistics_slug" class="col-sm-2 col-form-label">Statistics Slug</label>
+    <label for="statistics_stub" class="col-sm-2 col-form-label">Statistics Stub</label>
     <div class="col-sm-6">
-        <input type="text" class="form-control" id="statistics_slug" aria-describedby="statsHelp" placeholder="my-license">
+        <input type="text" class="form-control" name="statistics_stub" aria-describedby="statsHelp" placeholder="my-license">
         <small id="statsHelp" class="form-text text-muted">A unique string for use in generating reports for this license (no spaces).</small>
+    </div>
+</div>
+<div class="form-group row">
+    <label for="statistics_stub" class="col-sm-2 col-form-label">Cost</label>
+    <div class="col-sm-6 input-group">
+        <div class="input-group-prepend">
+            <div class="input-group-text">$</div>
+        </div>
+        <input type="text" class="form-control" name="cost" aria-describedby="statsHelp" placeholder="100000000000">
     </div>
 </div>
 <div class="form-group row">
     <label for="starts" class="col-sm-2 col-form-label">Start Date</label>
     <div class="col-sm-3">
-        <input type="date" class="form-control" id="starts" aria-describedby="startHelp" placeholder="{{ \Carbon\Carbon::now() }}">
+        <input type="date" class="form-control" name="starts" aria-describedby="startHelp" value="{{ \Carbon\Carbon::now() }}">
         <small id="startHelp" class="form-text text-muted">When the license starts.</small>
     </div>
 </div>
 <div class="form-group row">
-    <label for="ends" class="col-sm-2 col-form-label">End Date</label>
+    <label for="expires" class="col-sm-2 col-form-label">Expires Date</label>
     <div class="col-sm-3">
-        <input type="date" class="form-control" id="ends" aria-describedby="endHelp" placeholder="{{ \Carbon\Carbon::now() }}">
+        <input type="date" class="form-control" name="expires" aria-describedby="endHelp" placeholder="{{ \Carbon\Carbon::now() }}">
         <small id="endHelp" class="form-text text-muted">When the license expires.<br />Leave as-is for ones that don't expire.</small>
     </div>
 </div>
 <div class="form-group row">
     <label for="public" class="col-sm-2 col-form-label">Public Access</label>
     <div class="col-sm-3">
-        <input type="checkbox" class="form-control" id="public" aria-describedby="publicHelp">
+        <input type="checkbox" class="form-control" name="public" aria-describedby="publicHelp">
         <small id="publicHelp" class="form-text text-muted">Check if this should be public access (no Lcard required).</small>
     </div>
 </div>
