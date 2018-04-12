8479d041592f5996720a864c8c68a78a38a25b4a
Ryan Eby <eby@ryaneby.com>
2018-04-12 13:08:48 -0400

    allow most things to be fillable

--- app/License.php
+++ app/License.php
@@ -4,9 +4,14 @@ namespace App;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class License extends Model
 {
+    use SoftDeletes;
+
+    protected $fillable = [ 'vendor_id', 'cost', 'notes', 'statistics_stub', 'starts', 'expires' ];
+
     public function vendor()
     {
         return $this->belongsTo('App\Vendor');
