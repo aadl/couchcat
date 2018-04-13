8cb3c2a9fcb38b755bfd42a2f2f6a9ebb5891ddf
Ryan Eby <eby@ryaneby.com>
2018-04-13 10:15:15 -0400

    vendor dropdown and better dates

--- resources/views/license/create.blade.php
+++ resources/views/license/create.blade.php
@@ -16,16 +16,12 @@
 <div class="form-group row">
     <label for="vendor_id" class="col-sm-2 col-form-label">Vendor Name</label>
     <div class="col-sm-6">
-        <select type="text" class="form-control" name="vendor_id" aria-describedby="vendorHelp" placeholder="My Vendor">
-            @foreach ($vendors as $vendor)
-                <option value="{{ $vendor->id }}">{{ $vendor->name }}</option>
-            @endforeach
-        </select>
-        <small id="emailHelp" class="form-text text-muted">Create new vendors on the vendor page.</small>
+        {{ Form::select('vendor_id', $vendors, $vendor_id, ['class' => 'form-control', 'aria-describedby' => 'vendorHelp']) }}
+        <small id="vendorHelp" class="form-text text-muted">Create new vendors on the vendor page.</small>
     </div>
 </div>
 <div class="form-group row">
-    <label for="license_slug" class="col-sm-2 col-form-label">Statistics Stub</label>
+    <label for="license_slug" class="col-sm-2 col-form-label">License Slug</label>
     <div class="col-sm-6">
         <input type="text" class="form-control" name="license_slug" aria-describedby="statsHelp" placeholder="my-license">
         <small id="statsHelp" class="form-text text-muted">A unique string for use in generating reports and organizing for this license (no spaces).</small>
@@ -43,20 +39,20 @@
 <div class="form-group row">
     <label for="starts" class="col-sm-2 col-form-label">Start Date</label>
     <div class="col-sm-3">
-        <input type="date" class="form-control" name="starts" aria-describedby="startHelp" value="{{ \Carbon\Carbon::now() }}">
+        {{ Form::date('starts', \Carbon\Carbon::now(), ['class' => 'form-control', 'aria-describedby' => 'startHelp']) }}
         <small id="startHelp" class="form-text text-muted">When the license starts.</small>
     </div>
 </div>
 <div class="form-group row">
     <label for="expires" class="col-sm-2 col-form-label">Expires Date</label>
     <div class="col-sm-3">
-        <input type="date" class="form-control" name="expires" aria-describedby="endHelp" placeholder="{{ \Carbon\Carbon::now() }}">
+        {{ Form::date('expires', null, ['class' => 'form-control', 'aria-describedby' => 'endHelp']) }}
         <small id="endHelp" class="form-text text-muted">When the license expires.<br />Leave as-is for ones that don't expire.</small>
     </div>
 </div>
 <div class="form-group row">
     <label for="patrons_only" class="col-sm-2 col-form-label">Patrons Only</label>
-    <div class="col-sm-3">
+    <div class="col-sm-6">
         <input type="checkbox" class="form-control" name="patrons_only" aria-describedby="publicHelp" checked>
         <small id="publicHelp" class="form-text text-muted">Check if this should be public access (no Lcard required).</small>
     </div>
