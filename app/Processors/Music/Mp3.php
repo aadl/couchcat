2fc353bdc75b54089077a47ad4d8e181a9d835a3
Ryan Eby <eby@ryaneby.com>
2018-04-15 18:24:32 -0400

    rename and create zip for mp3

---
+++ app/Processors/Music/Mp3.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Processors\Music;
+
+use Illuminate\Support\Facades\Storage;
+use Comodojo\Zip\Zip;
+use FFMpeg;
+
+class Mp3
+{
+    public function __construct($doc)
+    {
+        $this->doc = $doc;
+        $this->couchid = $doc->_id;
+        $this->ffmpeg = FFMpeg\FFMpeg::create();
+    }
+
+    /**
+     * Rename the mp3 files to be URL friendly
+     *
+     * @return bool
+     */
+    public function fixMp3Filename($file)
+    {
+        $parsepath = pathinfo($file);
+        $filename = $parsepath['filename'];
+        $safe_filename = str_slug($filename, '-');
+        if ($parsepath['extension'] !== 'mp3') {
+            return false;
+        }
+        if ($filename !== $safe_filename) {
+            $old_file = $file;
+            $new_file = $parsepath['dirname'] . '/' . $safe_filename . '.flac';
+            if (Storage::move($old_file, $new_file)) {
+                return "Renamed $filename to $safe_filename";
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public function createMp3Zip()
+    {
+        $directory = storage_path('app/music/'.$this->couchid .'/derivatives/');
+        $zip_filename = $directory . $this->couchid . '.zip';
+        $zip_exists = Storage::exists('music/'.$this->couchid.'/derivatives/'.$this->couchid . '.zip');
+
+        abort_if($zip_exists, 500, 'MP3 zip file exists. Make sure this is correct.');
+
+        $zip = Zip::create($zip_filename);
+        $zip->setSkipped('HIDDEN');
+        $files = Storage::allFiles('music/'.$this->couchid.'/derivatives/');
+        $to_add = array();
+        foreach($files as $file) {
+            $parsepath = pathinfo($file);
+            if($parsepath['extension'] == 'mp3') {
+                $to_add[] = storage_path('app/'.$file);
+            }
+        }
+        print_r($to_add);
+        $zip->add($to_add);
+        if ($zip->close()) {
+            return "Created $zip_filename";
+        }
+    }
+}
