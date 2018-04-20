747adb6de6c3bb2eca316e40d983b0bf2529d2c7
Troy Leonard <troyanth@umich.edu>
2018-04-20 17:27:19 -0400

    js functions for adding tracks to a form and being able to remove them

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,3 +5,80 @@
  * application frontend using useful Laravel and JavaScript libraries.
  */
 
+
+$(function() {
+    function numTracks() {
+        return $('[id^=add-track-title]').length + 1;
+    };
+
+    function appendTrack() {
+        const trackCount = numTracks();
+        const appendTarget = $('#is_active').parents('.form-group');
+
+        // create new row for form elements
+        appendTarget.before($(document.createElement('div'))
+                    .addClass('form-group row track-title-' + trackCount + ' track-' + trackCount)
+        );
+        let target = $('.track-title-' + trackCount);
+
+        // add label and input for track title
+        target.append($(document.createElement('label'))
+              .html('Track Title ' + trackCount)
+              .addClass('col-sm-2 col-form-label')
+              .attr('for', 'add-track-title-' + trackCount)
+        );
+        target.append($(document.createElement('div'))
+              .addClass('col-sm-6')
+        );
+        target.children('div').append($(document.createElement('input')) 
+              .addClass('form-control')
+              .attr('id', 'add-track-title-' + trackCount)
+              .attr('name', 'track-title[]')
+              .attr('type', 'text')
+        );
+
+        // create new row for form elements
+        appendTarget.before($(document.createElement('div'))
+                    .addClass('form-group row track-file-' + trackCount + ' track-' + trackCount)
+        );
+        target = $('.track-file-' + trackCount);
+
+        // add label and input for track upload
+        target.append($(document.createElement('label'))
+              .html('Track File ' + trackCount)
+              .addClass('col-sm-2 col-form-label')
+              .attr('for', 'add-track-' + trackCount)
+        );
+        target.append($(document.createElement('div'))
+              .addClass('col-sm-6')
+        );
+        target.children('div').append($(document.createElement('input')) 
+              .addClass('form-control')
+              .attr('id', 'add-track-' + trackCount)
+              .attr('name', 'track-title[]')
+              .attr('type', 'file')
+        );
+
+        // create delete button for track
+        target.children('div').append($(document.createElement('button'))
+              .html('Delete Track ' + trackCount)
+              .addClass('btn btn-danger track-delete')
+              .attr('data-target', '.track-' + trackCount)
+        );
+    }
+
+    $('#mat_code').change(function() {
+        const matCode = $(this).val();
+        if (matCode == 'z' || matCode == 'za') {
+            appendTrack();
+        }
+    });
+
+    $('form').on('click', '.track-delete', function() {
+        const target = $(this).attr('data-target');
+        $(target).fadeOut();
+
+        return false;
+    });
+
+});
