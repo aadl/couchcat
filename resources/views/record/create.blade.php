ced3a23ee8dde7d91e190c696a21af878fd8d74e
Troy Leonard <troyanth@umich.edu>
2018-04-20 10:37:22 -0400

    pass ids to creation form builder options so labels work

--- resources/views/record/create.blade.php
+++ resources/views/record/create.blade.php
@@ -16,14 +16,14 @@
 <div class="form-group row">
     <label for="license_slug" class="col-sm-2 col-form-label">License Name</label>
     <div class="col-sm-6">
-        {{ Form::text('license_slug', $license_slug, ['class' => 'form-control', 'aria-describedby' => 'licenseHelp']) }}
+        {{ Form::text('license_slug', $license_slug, ['id' => 'license_slug', 'class' => 'form-control', 'aria-describedby' => 'licenseHelp']) }}
         <small id="licenseHelp" class="form-text text-muted">Create new licenses on the licenses page.</small>
     </div>
 </div>
 <div class="form-group row">
     <label for="title" class="col-sm-2 col-form-label">Title</label>
     <div class="col-sm-6">
-        {{ Form::text('title', null, ['class' => 'form-control', 'aria-describedby' => 'titleHelp']) }}
+        {{ Form::text('title', null, ['id' => 'title', 'class' => 'form-control', 'aria-describedby' => 'titleHelp']) }}
         <small id="titleHelp" class="form-text text-muted">Title of the record.</small>
     </div>
 </div>
@@ -37,28 +37,28 @@
 <div class="form-group row">
     <label for="pub_year" class="col-sm-2 col-form-label">Pub Year</label>
     <div class="col-sm-6">
-        {{ Form::number('pub_year', null, ['class' => 'form-control', 'aria-describedby' => 'pubYearHelp']) }}
+        {{ Form::number('pub_year', null, ['id' => 'pub_year', 'class' => 'form-control', 'aria-describedby' => 'pubYearHelp']) }}
         <small id="pubYearHelp" class="form-text text-muted">Year this material was published.</small>
     </div>
 </div>
 <div class="form-group row">
     <label for="cover" class="col-sm-2 col-form-label">Cover Image</label>
     <div class="col-sm-6">
-        {{ Form::file('cover', ['class' => 'form-control', 'aria-describedby' => 'coverHelp']) }}
+        {{ Form::file('cover', ['id' => 'cover', 'class' => 'form-control', 'aria-describedby' => 'coverHelp']) }}
         <small id="coverHelp" class="form-text text-muted">Attach a cover image.</small>
     </div>
 </div>
 <div class="form-group row">
     <label for="attachment" class="col-sm-2 col-form-label">Record File(s)</label>
     <div class="col-sm-6">
-        {{ Form::file('attachment', ['class' => 'form-control', 'aria-describedby' => 'attachmentHelp']) }}
+        {{ Form::file('attachment', ['id' => 'attachment', 'class' => 'form-control', 'aria-describedby' => 'attachmentHelp']) }}
         <small id="attachmentHelp" class="form-text text-muted">Attach a file relevant to the record (e.g., a pdf for a book download).</small>
     </div>
 </div>
 <div class="form-group row">
     <label for="is_active" class="col-sm-2 col-form-label">Record Active</label>
     <div class="col-sm-6">
-        {{ Form::checkbox('is_active', null, true, ['aria-describedby' => 'publicHelp']) }}
+        {{ Form::checkbox('is_active', null, true, ['id' => 'is_active', 'aria-describedby' => 'publicHelp']) }}
         <small id="publicHelp" class="form-text text-muted">Uncheck if this should suppressed in the public catalog.</small>
     </div>
 </div>
