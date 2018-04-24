86f0bd648493fb2a84e74ba5627525e02d93e8af
Troy Leonard <troyanth@umich.edu>
2018-04-24 18:40:43 -0400

    fix mat types in record constructor. get more of the typical couch fields into creation and editing. ksort album tracks if they aren't in track order

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -17,7 +17,7 @@ class RecordController extends Controller
     public function __construct()
     {
         $this->couch = resolve('Couchdb');
-        $this->mat_types = config('mat_types')['downloads'];
+        $this->mat_types = config('mat_types');
     }
 
     private function process_form_file_uploads($files, $id, $mat_code = '', $licensed_from = '') 
@@ -69,7 +69,7 @@ class RecordController extends Controller
      */
     public function create(Request $request)
     {
-        $mat_types = $this->mat_types;
+        $mat_types = $this->mat_types['downloads'];
         $license_slug = $request->input('license_slug') ?? '';
         return view('record.create', compact('mat_types', 'license_slug'));
     }
@@ -94,11 +94,14 @@ class RecordController extends Controller
         // create new record object and assign fields
         $record = new \stdClass;
         $record->_id = str_slug($input['title'], '-');
+        $record->bib_created = date('Y-m-d');
+        $record->bib_lastupdate = date('Y-m-d');
+        $record->title = $input['title'];
         $record->licensed_from = $input['license_slug'];
         $record->mat_code = $input['mat_code'];
         $record->pub_year = $input['pub_year'];
-        $record->active = ($input['is_active'] ? 1 : 0);
-        $record->notes = $input['notes'];
+        $record->active = (isset($input['is_active']) ? 1 : 0);
+        $record->notes = explode("\n\n", $input['notes']);
 
         // grab and upload the cover image if provided
         if (isset($input['cover'])) {
@@ -163,8 +166,12 @@ class RecordController extends Controller
      */
     public function edit($id)
     {
-        $mat_types = $this->mat_types;
+        $mat_types = array_merge($this->mat_types['physical'], $this->mat_types['downloads']);
         $record = $this->couch->getDoc($id);
+        if (isset($record->tracks)) {
+            $record->tracks = (array) $record->tracks;
+            ksort($record->tracks);
+        }
         return view('record.edit', compact('mat_types', 'record'));
     }
 
@@ -177,19 +184,60 @@ class RecordController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'title' => 'required',
+            'cover' => 'sometimes|mimes:jpg,jpeg'
+        ]);
+
         $input = $request->all();
         $record = $this->couch->getDoc($id);
         
+        $record->bib_lastupdate = date('Y-m-d');
         $record->licensed_from = $input['license_slug'];
         $record->title = $input['title'];
         $record->mat_code = $input['mat_code'];
-        $record->notes = $input['notes'];
+        $record->pub_year = $input['pub_year'];
+        $record->active = (isset($input['is_active']) ? 1 : 0);
+        $record->notes = explode("\n\n", $input['notes']);
+        if (isset($input['documentation'])) {
+            $record->documentation = explode("\n\n", $input['notes']);
+        }
+
+        if (isset($input['attachment'])) {
+            if ($input['mat_code'] == 'zb' || $input['mat_code'] == 'zp') {
+                $allowed = 'pdf';
+                $this->validate($request, [
+                    'attachment' => 'required|mimes:' . $allowed
+                ]);
+                $this->process_form_file_uploads($input['attachment'], $record->_id, $input['mat_code'], $record->licensed_from);
+            }
+        }
+
+        if (isset($input['track-file'])) {
+            $allowed = 'mp3';
+            $this->validate($request, [
+                'track-file' => 'required|mimes:' . $allowed
+            ]);
+            foreach ($input['track-file'] as $track) {
+                $track_title = explode('.', $track->getClientOriginalName())[0];
+                $track_num = (int) substr($track_title, 0, 2);
+                $track_title = substr($track_title, 2);
+                $record->tracks->$track_num = new \stdClass;
+                $record->tracks->$track_num->title = $track_title;
+            }
+        }
 
         try {
             $this->couch->storeDoc($record);
         } catch (Exception $e) {
             $this->error("Updating record failed : " . $e->getMessage());
         }
+
+        // tracks need to be processed after an initial record is already created
+        if (isset($input['track-file'])) {
+            $this->process_form_file_uploads($input['track-file'], $record->_id, $input['mat_code'], $record->licensed_from);
+        }
+
         // return redirect('record/' . $id);
     }
 
